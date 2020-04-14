578c1b110210c207b0a72fa2d0e67f9e10b53869
Sampaio Leal <samp14hd@gmail.com>
2020-04-14 15:01:10 -0300

    Updates

---
+++ precache-manifest.4ad7bcbe4495c60437b8197e8f095949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160d8b432c755cc352fda04bcfc6dd6a",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/index.html"
+  },
+  {
+    "revision": "1249c35e65b5dd8b9539",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/static/css/main.b732bf66.chunk.css"
+  },
+  {
+    "revision": "eedf6db057ee6feccb2a",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/static/js/2.e0f9d925.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/static/js/2.e0f9d925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1249c35e65b5dd8b9539",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/static/js/main.0fecaa4a.chunk.js"
+  },
+  {
+    "revision": "11a7e39d9c5c760c2103",
+    "url": "/app-web-denuncia-aglomeracao-quarentena/static/js/runtime-main.e350d2f6.js"
+  }
+]);
